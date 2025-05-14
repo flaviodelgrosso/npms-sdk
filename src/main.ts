78f386138d29a6b3dd58f9307a1a84c936e61784
Flavio Del Grosso <flaviodelgrosso@live.it>
2025-05-14 17:25:38 +0200

    feat(types): export additional types from search and package modules

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
+export type { SuggestionResult, SearchResult } from './types/search.ts';
+export type { PackageInfo, MultiPackageInfo } from './types/package.ts';
+
 export { default as NpmsIO } from './npms.ts';
